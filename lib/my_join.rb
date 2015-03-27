c82a9b5c8d3b280f681992017467a118b968c406
Katie Hoffman <katie@katie.local>
2015-03-27 00:05:57 -0400

    add solution

--- lib/my_join.rb
+++ lib/my_join.rb
@@ -1,1 +1,10 @@
-# your code goes here
+class Array
+	def my_join(arg="")
+		final_string = ""
+		self.each_with_index do |element, i|
+			final_string << arg if i > 0
+			final_string << element.to_s
+		end
+		final_string
+	end
+end
