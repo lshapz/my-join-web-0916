6e8a85d471db7df4398b1028a3c8af163ffc7304
lshapz <laurashapiro01@gmail.com>
2017-01-11 17:18:51 -0500

    Done.

--- lib/my_join.rb
+++ lib/my_join.rb
@@ -1,1 +1,19 @@
 # your code goes here
+
+class Array
+
+  def my_join(arg = '')
+    string = ""
+    last_index = self.length - 1 
+    self.each_with_index do |thing, index|
+      if index != last_index
+        string += (thing.to_s + arg.to_s)
+      else 
+        string += thing.to_s
+      end
+    end
+    string
+  end
+
+
+end
